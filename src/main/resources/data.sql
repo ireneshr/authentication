8665665577d993b36b13596c44befc42f6bcbd86
MauArg <maulp_gnt@hotmail.com>
2021-05-23 17:35:20 -0300

    Refactored to JDK11

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- Bcrypt Password generator & checker
 -- https://bcrypt-generator.com/
 
-insert into users values ('Irene', '$2y$18$e6vunphVC5BocMhUFWn04.0rQtlBl0f/8.3w8zePHTrLnOSRzLMpi', 'dummy', 1);
+insert into users values ('Irene', '$2y$18$e6vunphVC5BocMhUFWn04.0rQtlBl0f/8.3w8zePHTrLnOSRzLMpi', 'ireneshr.1994@gmail.com', 1);
 insert into authorities values ('Irene', 'ROLE_ADMIN');
 
-insert into users values ('Mauricio', '', '', 1);
+insert into users values ('Mauricio', '$2y$12$LmpsT2Jy2YVZOOcwaBsAp.NJ3mRIhXTH/W7KAHwhVQ3o2LudZKvcG ', 'maulp_gnt@hotmail.com', 1);
 insert into authorities values ('Mauricio', 'ROLE_ADMIN');
