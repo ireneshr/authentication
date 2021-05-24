fb8e44368c1ecf7efc8f5bdfa7435a346f6e26e2
MauArg <maulp_gnt@hotmail.com>
2021-05-24 00:12:19 -0300

    Automatization of docker push and kubernetes deployment

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM bigboards/java-8-armv7l
 COPY "./target/*.jar" "app.jar"
-ENTRYPOINT ["java","-Dspring.profiles.active=dev","-jar","app.jar"]
+ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,5 @@
 insert into users values ('Irene', '$2y$18$e6vunphVC5BocMhUFWn04.0rQtlBl0f/8.3w8zePHTrLnOSRzLMpi', 'ireneshr.1994@gmail.com', 1);
 insert into authorities values ('Irene', 'ROLE_ADMIN');
 
-insert into users values ('Mauricio', '$2y$12$LmpsT2Jy2YVZOOcwaBsAp.NJ3mRIhXTH/W7KAHwhVQ3o2LudZKvcG ', 'maulp_gnt@hotmail.com', 1);
+insert into users values ('Mauricio', '$2y$12$LmpsT2Jy2YVZOOcwaBsAp.NJ3mRIhXTH/W7KAHwhVQ3o2LudZKvcG', 'maulp_gnt@hotmail.com', 1);
 insert into authorities values ('Mauricio', 'ROLE_ADMIN');
