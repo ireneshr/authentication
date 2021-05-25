63609c35be2b3dd1308af475559d03529fb13d16
MauArg <maulp_gnt@hotmail.com>
2021-05-25 13:25:19 -0300

    Added scripts for local database creation

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,14 @@
+--User/DB creation
+/*
+create user 'micro-jcon'@'localhost' identified via mysql_native_password;
+
+grant all privileges on *.* to 'micro-jcon'@'localhost' with grant option;
+
+set password for 'micro-jcon'@'localhost' = password('w4miTXcHXCXL45pbUnTBeVg');
+
+create database authentication;
+*/
+
 CREATE TABLE users (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(100) NOT NULL,
