5623e509b742c86cc87dbad60c7325979995d0ea
Irene Hermosilla <ihermosilla@famar.delphiauto.net>
2021-07-04 21:13:53 -0300

    user changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- Bcrypt Password generator & checker
 -- https://bcrypt-generator.com/
 
-insert into users values ('Irene', '$2y$18$e6vunphVC5BocMhUFWn04.0rQtlBl0f/8.3w8zePHTrLnOSRzLMpi', 'ireneshr.1994@gmail.com', 1);
+insert into users values ('Irene', '$2y$12$tJdGRh8j02NFRNDGywqcge9N7Oo4j.iDdUjlCXc8hGPaTreikhOdy', 'ireneshr.1994@gmail.com', 1);
 insert into authorities values ('Irene', 'ROLE_ADMIN');
 
 insert into users values ('Mauricio', '$2y$12$LmpsT2Jy2YVZOOcwaBsAp.NJ3mRIhXTH/W7KAHwhVQ3o2LudZKvcG', 'maulp_gnt@hotmail.com', 1);
